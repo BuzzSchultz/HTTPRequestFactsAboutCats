182fddd2a7ce091e7c545998e68c918f3daa51c7
BuzzSchultz <sabacan@mail.ru>
2022-07-10 16:18:29 +0300

    Adding files to repository

---
+++ src/main/java/ru/homeworks/abatsanov/Main.java
@@ -0,0 +1,42 @@
+package ru.homeworks.abatsanov;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    private static final String URL =
+            "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();
+
+        HttpGet request = new HttpGet(URL);
+        CloseableHttpResponse response = httpClient.execute(request);
+
+        ObjectMapper mapper = new ObjectMapper();
+        List<FactsAboutCats> factsAboutCatsList =
+                mapper.readValue(response.getEntity().getContent(), new TypeReference<>() {
+                });
+
+        List<FactsAboutCats> votesNotNull = factsAboutCatsList.stream()
+                .filter(f -> f.getUpvotes() != null)
+                .filter(f -> Integer.parseInt(f.getUpvotes()) > 0)
+                .collect(Collectors.toList());
+        votesNotNull.forEach(System.out::println);
+    }
+}
